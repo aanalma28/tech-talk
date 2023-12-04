f13dafe10a6c6586e5a9f066f97e1f2d1334b4e9
Aan Alma <aanalma03@gmail.com>
2023-12-04 22:44:20 +0700

    test express with mongoose

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const MongoClient = require('mongodb').MongoClient
 const mongoose = require('mongoose')
+
 const next = require('next');
 
 const dev = process.env.NODE_ENV !== 'production';
@@ -29,41 +30,28 @@ const User = mongoose.model('User', userSchema)
 app.prepare().then(() => {
   const server = express(); 
 
-  // connect to db
-  MongoClient.connect('mongodb://localhost:27017/techtalk-db', (err, client) => {
-    if(err) throw err
-
-    const db = client.db('techtalk-db')
-
-    db.collection('users').find().toArray((err, result) => {
-      if(err) throw err
-
-      console.log(result)
-    })
-  })
-
   server.use(express.json()) // for parsing application/json
   server.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
   // Custom Express routes
   server.post('/test', async (req, res) => {
-    try{
-      const data = await req.body
+    const getData = await User.find()
+    res.status(200).json(getData)
+    // try{
+    //   const data = await req.body
 
+    //   res.status(200).json({
+    //       message: "Data send!",
+    //       success: true,
+    //       data: req.body
+    //   })
 
-
-      res.status(200).json({
-          message: "Data send!",
-          success: true,
-          data: req.body
-      })
-
-    }catch(e){
-      res.status(500).json({
-        message: "Invalid",
-        success: false
-      })
-    }
+    // }catch(e){
+    //   res.status(500).json({
+    //     message: "Invalid",
+    //     success: false
+    //   })
+    // }
   });
 
 //   Handle Next.js routes
@@ -72,8 +60,26 @@ app.prepare().then(() => {
   });
 
   // Start Express server
-  server.listen(3000, (err) => {    
-    if (err) throw err;
-    console.log('> Ready on http://localhost:3000');
+  server.listen(3000, async (err) => {    
+    // // connect to db
+    // MongoClient.connect('mongodb://localhost:27017/techtalk-db', (err, client) => {
+    //   if(err) throw err
+
+    //   const db = client.db('techtalk-db')
+
+    //   db.collection('users').find().toArray((err, result) => {
+    //     if(err) throw err
+
+    //     console.log(result)
+    //   })
+    // })
+    try{
+      await mongoose.connect('mongodb://localhost:27017/techtalk-db')
+      if (err) throw err;
+      console.log('> Ready on http://localhost:3000');
+    }catch(err){
+      console.error(err)
+    }
+
   });
 });
